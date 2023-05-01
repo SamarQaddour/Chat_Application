75976b74fc9c823cbe49c549efeca5ecbf7f0a1d
samar qaddour <samarqaddour051@gamil.com>
2023-05-01 13:28:40 -0700

    make style for responsive

--- src/components/Massege.js
+++ src/components/Massege.js
@@ -1,10 +1,16 @@
 import React from 'react'
-
+import samarImage from '../img/samarImage.jpeg';
+import jacket from '../img/jecket.jpg'
 const Massege = () => {
     return (
-        <div className='massege'>
+        <div className='massege owner'>
+            <div className='messageInfo'>
+                <img src={samarImage} alt='...' />
+                <span>Just Now</span>
+            </div>
             <div className='messageContent'>
-                messageContent
+                <p>hello</p>
+                <img src={jacket} alt='...' />
             </div>
         </div>
     )
